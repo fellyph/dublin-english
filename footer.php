504debcd4b44153527062733e645d46ac0e6e3a8
fellyph <fellyph.cintra@gmail.com>
2018-05-19 19:56:23 +0100

    adding style footer

--- footer.php
+++ footer.php
@@ -1,5 +1,13 @@
-  <footer>
+  <footer class="footer">
+    <div class="footer__address">
+      <p>Dublin</p>
+      <p>email</p>
+      <p>phone number</p>
+    </div>
     Dublin English School
+    <div class="footer__map">
+      
+    </div>
   </footer>
   <?php wp_footer(); ?>
 </body>
