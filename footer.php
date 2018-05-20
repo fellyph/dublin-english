5176a29287dfbb701800e2ed476b518ec2e2dc8a
fellyph <fellyph.cintra@gmail.com>
2018-05-21 00:54:21 +0100

    adding menu on footert

--- footer.php
+++ footer.php
@@ -1,12 +1,15 @@
   <footer class="footer">
-    <div class="footer__address">
-      <p>Dublin</p>
-      <p>email</p>
-      <p>phone number</p>
+    <div class="footer__widget footer__address">
+      <h3>Contact</h3>
+      <p>Dublin, Ireland</p>
+      <p>test@dublin.school</p>
+      <p>99 999 9999</p>
     </div>
     Dublin English School
-    <div class="footer__map">
-      
+    <div class="footer__widget footer__links">
+    <?php wp_nav_menu(array(
+            'theme_location' => 'main-menu',
+            'menu_class' => 'top-bar__menu') ); ?>
     </div>
   </footer>
   <?php wp_footer(); ?>
